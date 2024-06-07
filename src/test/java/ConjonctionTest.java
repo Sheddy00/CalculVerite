f0d8aa398655816eab2923a356d87007b307da73
Sheddy00 <hei.sheddy.2@gmail.com>
2024-06-07 16:54:00 +0300

    feat: ConjonctionTest

--- src/test/java/ConjonctionTest.java
+++ src/test/java/ConjonctionTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class ConjonctionTest {
+import hei.school.Affirmation;
+import hei.school.Conjonction;
+import hei.school.ConjonctionType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConjonctionTest {
+    @Test
+    public void testConjonctionEt() {
+        Affirmation aff1 = new Affirmation("Lou est beau ", true);
+        Affirmation aff2 = new Affirmation("Lou est pauvre", false);
+
+        Conjonction conjonction = new Conjonction(ConjonctionType.DONC, aff1, aff2);
+        assertFalse(conjonction.evaluation());
+    }
+
+    @Test
+    public void testConjonctionOu() {
+        Affirmation aff1 = new Affirmation("Lou est pauvre ", false);
+        Affirmation aff2 = new Affirmation("Lou est beau", true);
+
+        Conjonction conjonction = new Conjonction(ConjonctionType.OU, aff1, aff2);
+        assertTrue(conjonction.evaluation());
+    }
+
+    @Test
+    public void testConjonctionDonc() {
+        Affirmation aff1 = new Affirmation("Lou est beau", true);
+        Affirmation aff2 = new Affirmation("Lou est pauvre", false);
+
+        Conjonction conjonction = new Conjonction(ConjonctionType.DONC, aff1,aff2);
+        assertFalse(conjonction.evaluation());
+    }
+
 }
