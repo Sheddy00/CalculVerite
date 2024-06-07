2a16331398f5bd0069d61f79a9fe23ba93e09efb
Sheddy00 <hei.sheddy.2@gmail.com>
2024-06-07 16:51:37 +0300

    feat: Conjonction class

--- src/main/java/hei/school/Conjonction.java
+++ src/main/java/hei/school/Conjonction.java
@@ -1,2 +1,27 @@
-package hei.school;public class Conjonction {
+package hei.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Conjonction {
+    private ConjonctionType conjonctionType;
+    private Affirmation affirmation1;
+    private Affirmation affirmation2;
+
+    public boolean evaluation() {
+        switch (conjonctionType) {
+            case ET:
+                return affirmation1.getValeur() && affirmation2.getValeur();
+            case OU:
+                return affirmation1.getValeur() || affirmation2.getValeur();
+            case DONC:
+                return !affirmation1.getValeur() || affirmation2.getValeur();
+            default:
+                return false;
+        }
+    }
 }
